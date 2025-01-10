10d0949c0c7bb8fb9850ced6cc0295352046a4c5
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-01-11 01:06:24 +0800

    Start work on interests page

--- src/app/interests/page.jsx
+++ src/app/interests/page.jsx
@@ -1,13 +1,66 @@
 // Chin Ray wrote this
 "use client"
+
+import { useState, useEffect } from "react";
 import Header from "@/components/Header";
 import AddFeed from "@/components/Feeds/AddFeed";
+import { logoutSession, getUserInfoFromFirebaseAuth } from "@/lib/session";
+import styles from "./page.module.css";
 
 export default function Interests() {
-    return (
-        <div className="p-6 bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen">
-            <Header />
-            <AddFeed />
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userData = await getUserInfoFromFirebaseAuth();
+      if (!userData) {
+        setIsAuthenticated(false);
+        setIsLoading(false);
+        window.location.href = "/login";
+        return;
+      }
+      setUser(userData.decodedToken);
+      setIsAuthenticated(true);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!isAuthenticated && isLoading) return <p>LOADING...</p>;
+
+  return (
+    <div className="p-6 bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen">
+      <Header
+        user={user}
+        onSignOut={async () => {
+          await logoutSession();
+          window.location.href = "/login";
+        }}
+      />
+      <AddFeed />
+      <RenderInterestSelection />
+    </div>
+  )
+}
+
+function RenderInterestSelection() {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200">
+      <h2 className="text-lg font-semibold text-gray-800 mb-4">Interests</h2>
+      <div className={styles.container}>
+        <div>
+
         </div>
-    )
+        <div>
+          <h3>Tom's Hardware</h3>
+        </div>
+        <div>
+          <input type="checkbox" id="tomshardwarecheckbox"></input>
+        </div>
+      </div>
+    </div>
+  )
 }
