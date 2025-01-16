78c3085b215ecb3d1ee365f91058e62d5171bfe4
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-01-17 00:35:51 +0800

    Known subscribed interests will show as ticked under preset interests

--- src/app/interests/page.jsx
+++ src/app/interests/page.jsx
@@ -10,6 +10,7 @@ import Header from "@/components/Header";
 import styles from "./page.module.css";
 
 import { CiGlobe } from "react-icons/ci";
+import { set } from "date-fns";
 
 export default function Interests() {
   const [user, setUser] = useState(null);
@@ -20,20 +21,24 @@ export default function Interests() {
   const [categoryList, setCategoryList] = useState([]);
   const [error, setError] = useState(null);
 
-  const presetFeeds = [
+  const [presetFeeds, setPresetFeeds] = useState([
     {
       name: "Tom's Hardware",
       url: "https://www.tomshardware.com/rss.xml",
       categories: ["Technology", "Hardware"],
-      image: "images/TomsHardware.png"
+      image: "images/TomsHardware.png",
+      description: "An online publication focused on computer hardware and technology.",
+      checked: false
     },
     {
       name: "HardwareZone",
-      url: "https://www.hardwarezone.com.sg/feeds",
+      url: "https://feeds.feedburner.com/hardwarezone/all",
       categories: ["Technology", "Hardware"],
-      image: "images/HardwareZone.png"
+      image: "images/HardwareZone.png",
+      description: "An Singaporean online publication and forum focused on computer hardware and technology.",
+      checked: false
     }
-  ]
+  ])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -69,7 +74,20 @@ export default function Interests() {
 
   if (!isAuthenticated && isLoading) return <p>LOADING...</p>;
 
-feeds.forEach((feed) => console.log(feed.url));
+  let feedsTemp = feeds;
+  let presetFeedsTemp = presetFeeds;
+
+  console.log(presetFeedsTemp);
+  
+  for (let i = 0; i < feedsTemp.length; i++) {
+    for (let presetFeed of presetFeedsTemp) {
+      if (feeds[i].url === presetFeed.url) {
+        presetFeed.checked = true;
+        feedsTemp.splice(i, 1);
+      }
+    }
+  }
+
   return (
     <div className="p-6 bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen">
       <Header
@@ -81,13 +99,23 @@ feeds.forEach((feed) => console.log(feed.url));
         showSearchBar={false}
       />
       <AddFeed />
+      <RenderSubscribedInterests feeds={feedsTemp} />
+      <RenderInterestSelection presetFeeds={presetFeedsTemp} />
+    </div>
+  )
+}
+
+// Already subscribed interests section (pull from Firebase)
+function RenderSubscribedInterests({ feeds }) {
+  return (
+    <>
       <h2 className="text-lg font-semibold text-gray-800 mb-4">Subscribed Interests</h2>
       <div className="flex flex-row flex-wrap">
         {feeds.map((feed) => (
-          <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`}>
+          <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`} key={feed.url}>
             <div className={styles.container}>
               {/* <img src="images/TomsHardware.png" alt="Tom's Hardware" className={styles.image} /> */}
-              <CiGlobe className={styles["image-small"]}/>
+              <CiGlobe className={styles["image-small"]} />
               <div className={styles.centrediv}>
                 <h3 className={styles.interestname}>{feed.url.toString().replace("https://", "").split("/")[0].split(".").slice(-2).join(".")}</h3>
                 <p>{feed.url}</p>
@@ -99,43 +127,33 @@ feeds.forEach((feed) => console.log(feed.url));
           </div>
         ))}
       </div>
-      <RenderInterestSelection />
-    </div>
+    </>
   )
 }
 
-function RenderInterestSelection() {
+// Preset interests selection section
+function RenderInterestSelection({ presetFeeds }) {
   return (
     <>
       <h2 className="text-lg font-semibold text-gray-800 mb-4">Interests</h2>
       <div className="flex flex-row flex-wrap">
-        <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`}>
-          <div className={styles.container}>
-            <img src="images/TomsHardware.png" alt="Tom's Hardware" className={styles.image} />
-            <div className={styles.centrediv}>
-              <h3 className={styles.interestname}>Tom's Hardware</h3>
-              <p className={styles.description}>An online publication focused on computer hardware and technology.</p>
-              <p>https://www.tomshardware.com/rss.xml</p>
+        {
+          presetFeeds.map((feed) => (
+            <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`} key={feed.url}>
+              <div className={styles.container}>
+                <img src={feed.image} alt={feed.name} className={styles.image} />
+                <div className={styles.centrediv}>
+                  <h3 className={styles.interestname}>{feed.name}</h3>
+                  <p className={styles.description}>{feed.description}</p>
+                  <p>{feed.url}</p>
+                </div>
+                <div className={styles.centrediv}>
+                  <input type="checkbox" checked={feed.checked} id={`${feed.name.toLowerCase().replace(/\s/g, "").replace("'", "").replace("\"", "")}checkbox`} className={styles.checkbox}></input>
+                </div>
+              </div>
             </div>
-            <div className={styles.centrediv}>
-              <input type="checkbox" id="tomshardwarecheckbox" className={styles.checkbox}></input>
-            </div>
-          </div>
-        </div>
-
-        <div className="bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6">
-          <div className={styles.container}>
-            <img src="images/HardwareZone.png" alt="HardwareZone" className={styles.image} />
-            <div className={styles.centrediv}>
-              <h3 className={styles.interestname}>HardwareZone</h3>
-              <p className={styles.description}>An Singaporean online publication and forum focused on computer hardware and technology.</p>
-              <p>https://www.hardwarezone.com.sg/feeds</p>
-            </div>
-            <div className={styles.centrediv}>
-              <input type="checkbox" id="hardwarezonecheckbox" className={styles.checkbox}></input>
-            </div>
-          </div>
-        </div>
+          ))
+        }
       </div>
     </>
   )
