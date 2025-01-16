15959fd667215ab79a99b145aeb867379e14a0e3
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-01-16 22:46:19 +0800

    Managed to get subscribed interests to show

--- src/app/interests/page.jsx
+++ src/app/interests/page.jsx
@@ -4,14 +4,36 @@
 
 import { useState, useEffect } from "react";
 import { getUserInfoFromFirebaseAuth } from "@/lib/session";
+import { getFeedsFromDatabase } from "@/lib/firebase/feed_database";
 import AddFeed from "@/app/interests/AddFeed";
 import Header from "@/components/Header";
 import styles from "./page.module.css";
+
+import { CiGlobe } from "react-icons/ci";
 
 export default function Interests() {
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [feeds, setFeeds] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+  const [error, setError] = useState(null);
+
+  const presetFeeds = [
+    {
+      name: "Tom's Hardware",
+      url: "https://www.tomshardware.com/rss.xml",
+      categories: ["Technology", "Hardware"],
+      image: "images/TomsHardware.png"
+    },
+    {
+      name: "HardwareZone",
+      url: "https://www.hardwarezone.com.sg/feeds",
+      categories: ["Technology", "Hardware"],
+      image: "images/HardwareZone.png"
+    }
+  ]
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,14 +46,30 @@ export default function Interests() {
       }
       setUser(userData.decodedToken);
       setIsAuthenticated(true);
-      setIsLoading(false);
     };
 
     fetchData();
+
+    const fetchFeeds = async () => {
+      try {
+        const data = await getFeedsFromDatabase();
+        // const categories = data
+        //   .flatMap((feed) => feed.categories || [])
+        //   .filter((value, index, self) => self.indexOf(value) === index);
+        setFeeds(data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchFeeds();
   }, []);
 
   if (!isAuthenticated && isLoading) return <p>LOADING...</p>;
 
+feeds.forEach((feed) => console.log(feed.url));
   return (
     <div className="p-6 bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen">
       <Header
@@ -43,7 +81,24 @@ export default function Interests() {
         showSearchBar={false}
       />
       <AddFeed />
-      <SubscribedInterests />
+      <h2 className="text-lg font-semibold text-gray-800 mb-4">Subscribed Interests</h2>
+      <div className="flex flex-row flex-wrap">
+        {feeds.map((feed) => (
+          <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`}>
+            <div className={styles.container}>
+              {/* <img src="images/TomsHardware.png" alt="Tom's Hardware" className={styles.image} /> */}
+              <CiGlobe className={styles["image-small"]}/>
+              <div className={styles.centrediv}>
+                <h3 className={styles.interestname}>{feed.url.toString().replace("https://", "").split("/")[0].split(".").slice(-2).join(".")}</h3>
+                <p>{feed.url}</p>
+              </div>
+              <div className={styles.centrediv}>
+                <input type="checkbox" checked className={styles.checkbox}></input>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
       <RenderInterestSelection />
     </div>
   )
@@ -56,7 +111,7 @@ function RenderInterestSelection() {
       <div className="flex flex-row flex-wrap">
         <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`}>
           <div className={styles.container}>
-            <img src="images/TomsHardware.png" alt="Tom's Hardware" className={styles.image}/>
+            <img src="images/TomsHardware.png" alt="Tom's Hardware" className={styles.image} />
             <div className={styles.centrediv}>
               <h3 className={styles.interestname}>Tom's Hardware</h3>
               <p className={styles.description}>An online publication focused on computer hardware and technology.</p>
@@ -70,30 +125,16 @@ function RenderInterestSelection() {
 
         <div className="bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6">
           <div className={styles.container}>
-            <img src="images/HardwareZone.png" alt="HardwareZone" className={styles.image}/>
+            <img src="images/HardwareZone.png" alt="HardwareZone" className={styles.image} />
             <div className={styles.centrediv}>
               <h3 className={styles.interestname}>HardwareZone</h3>
               <p className={styles.description}>An Singaporean online publication and forum focused on computer hardware and technology.</p>
               <p>https://www.hardwarezone.com.sg/feeds</p>
             </div>
             <div className={styles.centrediv}>
-              <input type="checkbox" id="tomshardwarecheckbox" className={styles.checkbox}></input>
+              <input type="checkbox" id="hardwarezonecheckbox" className={styles.checkbox}></input>
             </div>
           </div>
-        </div>
-      </div>
-    </>
-  )
-}
-
-function SubscribedInterests() {
-  return (
-    <>
-      <h2 className="text-lg font-semibold text-gray-800 mb-4">Subscribed Interests</h2>
-      <div className="bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200">
-        
-        <div className={styles.container}>
-          
         </div>
       </div>
     </>
