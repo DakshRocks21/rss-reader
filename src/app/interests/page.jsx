0f51d7cee6ae3ef81e4f750af57babe2fd1ca36b
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-01-26 22:40:19 +0800

    Add filters, code cleanup

--- src/app/interests/page.jsx
+++ src/app/interests/page.jsx
@@ -4,12 +4,12 @@
 
 import { useState, useEffect } from "react";
 import { getUserInfoFromFirebaseAuth } from "@/lib/session";
-import { getFeedsFromDatabase, addFeedToDatabase, removeFeedFromDatabase } from "@/lib/firebase/feed_database";
+import { getFeedsFromDatabase } from "@/lib/firebase/feed_database";
 import AddFeed from "@/app/interests/AddFeed";
 import Header from "@/components/Header";
-import styles from "./page.module.css";
 
-import { CiGlobe } from "react-icons/ci";
+import { RenderSubscribedInterests, RenderInterestSelection } from "./RenderInterests";
+import { Categories } from "./Filters";
 
 export default function Interests() {
   const [user, setUser] = useState(null);
@@ -18,6 +18,7 @@ export default function Interests() {
 
   const [feeds, setFeeds] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
+  const [categoryFilterList, setCategoryFilterList] = useState([]);
   const [error, setError] = useState(null);
 
   const [presetFeeds, setPresetFeeds] = useState([
@@ -32,7 +33,7 @@ export default function Interests() {
     {
       name: "HardwareZone",
       url: "https://feeds.feedburner.com/hardwarezone/all",
-      categories: ["Technology", "Hardware"],
+      categories: ["Technology", "Hardware", "Singapore"],
       image: "images/HardwareZone.png",
       description: "An Singaporean technology forum and news outlet specialising in hardware.",
       checked: false
@@ -58,10 +59,11 @@ export default function Interests() {
     const fetchFeeds = async () => {
       try {
         const data = await getFeedsFromDatabase();
-        // const categories = data
-        //   .flatMap((feed) => feed.categories || [])
-        //   .filter((value, index, self) => self.indexOf(value) === index);
+        const categories = data
+          .flatMap((feed) => feed.categories || [])
+          .filter((value, index, self) => self.indexOf(value) === index);
         setFeeds(data);
+        setCategoryList(categories);
         setIsLoading(false);
       } catch (err) {
         setError(err.message);
@@ -74,22 +76,29 @@ export default function Interests() {
 
   if (!isAuthenticated && isLoading) return <p>LOADING...</p>;
 
-  let feedsTemp = feeds;
-  let presetFeedsTemp = presetFeeds;
-
-  // Check respective preset feed checkboxes if user is already subscribed to them, remove duplicates
-  for (let i = 0; i < feedsTemp.length; i++) {
-    for (let presetFeed of presetFeedsTemp) {
-      if (feeds[i].url === presetFeed.url) {
-        presetFeed.checked = true;
-        feedsTemp.splice(i, 1);
+  const populateFeeds = () => {
+    // Check respective preset feed checkboxes if user is already subscribed to them, remove duplicates
+    for (let i = 0; i < feeds.length; i++) {
+      for (let presetFeed of presetFeeds) {
+        if (feeds[i].url === presetFeed.url) {
+          presetFeed.checked = true;
+          feeds.splice(i, 1);
+        }
       }
     }
+
+    console.log(presetFeeds)
+
+    feeds.forEach(feed => feed.checked = true); // Check all remaining feeds (since they are subscribed if they are already in the database)
   }
 
-  console.log(presetFeedsTemp)
-
-  feedsTemp.forEach(feed => feed.checked = true); // Check all remaining feeds (since they are subscribed if they are already in the database)
+  const handleCategoryFilterChange = (category) => {
+    if (categoryFilterList.includes(category)) {
+      setCategoryFilterList(categoryFilterList.filter(item => item !== category));
+    } else {
+      setCategoryFilterList([...categoryFilterList, category]);
+    }
+  };
 
   return (
     <div className="p-6 bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen">
@@ -101,106 +110,11 @@ export default function Interests() {
         }}
         isOnHomePage={false}
       />
-      <AddFeed onAddFeed={() => window.location.reload()}/>
-      <RenderSubscribedInterests feeds={feedsTemp} />
-      <RenderInterestSelection presetFeeds={presetFeedsTemp} />
+      {populateFeeds()}
+      <AddFeed onAddFeed={() => window.location.reload()} />
+      <Categories categoryList={categoryList} onCategoryChange={(category) => handleCategoryFilterChange(category)} />
+      <RenderSubscribedInterests feeds={feeds} filter={categoryFilterList} />
+      <RenderInterestSelection presetFeeds={presetFeeds} filter={categoryFilterList} />
     </div>
   )
-}
-
-// Already subscribed interests section (pull from Firebase)
-function RenderSubscribedInterests({ feeds }) {
-  return (
-    <>
-      <h2 className="text-lg font-semibold text-gray-800 mb-4">Subscribed Interests</h2>
-      <div className="flex flex-row flex-wrap">
-        {feeds.map((feed) => (
-          <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`} key={feed.url}>
-            <div className={styles.container}>
-              {feed.name && feed.name != "" ? (
-                <>
-                  {
-                    feed.image && feed.image != "" ? (
-                      <img src={feed.image} alt={feed.name} className={styles["image"]} />
-                    ) : (
-                      <CiGlobe className={styles["image"]} />
-                    )
-                  }
-                  <div className={styles.centrediv}>
-                    <h3 className={styles.interestname}>{feed.name}</h3>
-                    <p className={styles.description}>{feed.description}</p>
-                    <p>{feed.url}</p>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <CiGlobe className={styles["image"]} />
-                  <div className={styles.centrediv}>
-                    <h3 className={styles.interestname}>{feed.url.toString().replace("https://", "").split("/")[0].split(".").slice(-2).join(".")}</h3>
-                    <p>{feed.url}</p>
-                  </div>
-                </>
-              )}
-
-              <div className={styles.rightalign}>
-                <Checkbox feed={feed} feeds={feeds} />
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  )
-}
-
-// Preset interests selection section
-function RenderInterestSelection({ presetFeeds }) {
-  return (
-    <>
-      <h2 className="text-lg font-semibold text-gray-800 mb-4">Interests</h2>
-      <div className="flex flex-row flex-wrap">
-        {
-          presetFeeds.map((feed) => (
-            <div className={`bg-white p-6 rounded-lg shadow-md mb-6 border border-gray-200 w-fit max-w-2xl mr-6 flex items-center`} key={feed.url}>
-              <div className={styles.container}>
-                <img src={feed.image} alt={feed.name} className={styles["image"]} />
-                <div className={styles.centrediv}>
-                  <h3 className={styles.interestname}>{feed.name}</h3>
-                  <p className={styles.description}>{feed.description}</p>
-                  <p>{feed.url}</p>
-                </div>
-                <div className={styles.centrediv}>
-                  <Checkbox feed={feed} feeds={presetFeeds} />
-                </div>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-    </>
-  )
-}
-
-function Checkbox({ feed, feeds }) {
-  const [checked, setChecked] = useState(false);
-  useEffect(() => setChecked(feed.checked), [feed.checked]);
-
-  function HandleCheckboxChange(event, url, feedsTemp) {
-    if (event.target.checked == true) {
-      console.log("Adding", url);
-      let feed = feedsTemp.find(x => x.url === url);
-      feed.checked = true;
-      addFeedToDatabase({ name: feed.name, feedUrl: feed.url, categories: feed.categories, image: feed?.image ?? "", description: feed?.description ?? "" });
-    } else {
-      console.log("Removing", url);
-      feedsTemp.find(x => x.url === url).checked = false;
-      // } else {
-      //   feedsTemp.splice(feedsTemp.indexOf(feedsTemp.find(x => x.url === url)), 1);
-      // }
-      removeFeedFromDatabase(url);
-    }
-    setChecked(event.target.checked);
-  }
-
-  return <input type="checkbox" checked={checked} onChange={event => HandleCheckboxChange(event, feed.url, feeds)} className={styles.checkbox}></input>
 }
