41974b3d960c3a69167c922c4c9fcd2d943b7661
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-01-26 22:40:19 +0800

    Add filters, code cleanup

---
+++ src/app/interests/Checkbox.jsx
@@ -0,0 +1,31 @@
+// Chin Ray wrote this
+
+"use client"
+
+import { useState, useEffect } from "react";
+import styles from "./page.module.css";
+import { addFeedToDatabase, removeFeedFromDatabase } from "@/lib/firebase/feed_database";
+
+export function Checkbox({ feed, feeds }) {
+    const [checked, setChecked] = useState(false);
+    useEffect(() => setChecked(feed.checked), [feed.checked]);
+  
+    function HandleCheckboxChange(event, url, feedsTemp) {
+      if (event.target.checked == true) {
+        console.log("Adding", url);
+        let feed = feedsTemp.find(x => x.url === url);
+        feed.checked = true;
+        addFeedToDatabase({ name: feed.name, feedUrl: feed.url, categories: feed.categories, image: feed?.image ?? "", description: feed?.description ?? "" });
+      } else {
+        console.log("Removing", url);
+        feedsTemp.find(x => x.url === url).checked = false;
+        // } else {
+        //   feedsTemp.splice(feedsTemp.indexOf(feedsTemp.find(x => x.url === url)), 1);
+        // }
+        removeFeedFromDatabase(url);
+      }
+      setChecked(event.target.checked);
+    }
+  
+    return <input type="checkbox" checked={checked} onChange={event => HandleCheckboxChange(event, feed.url, feeds)} className={styles.checkbox}></input>
+  }
