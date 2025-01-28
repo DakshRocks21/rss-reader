e85b83feea4c80999d01692217d471e9f9a92478
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-01-28 23:52:32 +0800

    Fix small issue in theming

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ export default function Header({
   }
 
   return (
-    // Chin Ray: Made header rounded, anchor to top of page, implement search bar show/hide
+    // Chin Ray: Made header rounded, anchor to top of page, implement search bar show/hide, theming
     <div className="bg-secondary-container shadow-lg p-4 flex items-center justify-between rounded-lg sticky top-0">
       <h1 className="text-xl font-bold text-on-secondary-container">
         <a href="/">RSS Feeds</a>
@@ -76,7 +76,7 @@ export default function Header({
               className="rounded-full"
             />
           ) : (
-            <FaUserCircle className="text-2xl text-on-primary" />
+            <FaUserCircle className="text-2xl text-on-secondary-container" />
           )}
         </div>
 
